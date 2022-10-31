da5e8f811aaea40782f399cef6fb9af15457b260
MDNET-Developer <qarabagli.murad@mail.ru>
2022-10-31 09:03:00 +0400

    UILayer.Update

---
+++ Dapper.WebAppCV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dapper.WebAppCV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
